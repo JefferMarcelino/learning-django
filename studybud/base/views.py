8c53c3e130830fb3f728512b42739cb9c714772a
JefferMarcelino <jeffersunde72@gmail.com>
2022-05-23 11:05:26 +0200

    using database

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,6 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
@@ -10,16 +12,15 @@ rooms = [
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
-    room = None
-
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
-    
+    room = Room.objects.get(id=pk)
     context = {"room": room}
-
     return render(request, "base/room.html", context)
+
+def createRoom(request):
+    context = {}
+    return render(request, "base/room_form.html", context)
