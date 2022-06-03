b5cef1516f99b54c81061565ff061c4cc9e62ea0
JefferMarcelino <jeffersunde72@gmail.com>
2022-06-03 16:56:12 +0200

    Recent activity

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -30,8 +30,11 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     created = models.TimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-updated", "-created")
 
     def __str__(self):
         return str(self.body[0:50])
